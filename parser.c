f2998d2ab3545fbddca2acfc7ac53ddafbe5632a
muthoni kinyua <joskin074@gmail.com>
2023-05-24 15:10:03 -0700

    simple shell

--- parser.c
+++ parser.c
@@ -1,33 +1,34 @@
 #include "shell.h"
 
 /**
- * is_cmd - determines if a sile is an executavle command
- * @info: the info struct 
- * @path: path to the file 
- * Return: 1 if true and 0otherwise 
+ * is_cmd - determines if a file is an executable command
+ * @info: the info struct
+ * @path: path to the file
+ *
+ * Return: 1 if true, 0 otherwise
  */
-
 int is_cmd(info_t *info, char *path)
 {
 	struct stat st;
 
 	(void)info;
-	if (!path ||stat(path, &st))
+	if (!path || stat(path, &st))
 		return (0);
 
 	if (st.st_mode & S_IFREG)
 	{
-		return(1);
+		return (1);
 	}
 	return (0);
 }
 
 /**
- * dup_chars - duplicates cahracters
+ * dup_chars - duplicates characters
  * @pathstr: the PATH string
  * @start: starting index
  * @stop: stopping index
- * Return: pointer to anew buffer
+ *
+ * Return: pointer to new buffer
  */
 char *dup_chars(char *pathstr, int start, int stop)
 {
@@ -35,18 +36,19 @@ char *dup_chars(char *pathstr, int start, int stop)
 	int i = 0, k = 0;
 
 	for (k = 0, i = start; i < stop; i++)
-		if (pathstr[1] != ':')
+		if (pathstr[i] != ':')
 			buf[k++] = pathstr[i];
 	buf[k] = 0;
- 	return (buf);
+	return (buf);
 }
 
 /**
  * find_path - finds this cmd in the PATH string
- * @info: the info struct 
+ * @info: the info struct
  * @pathstr: the PATH string
- * @cmd: the cmd to find 
- * Return: full path of the cmd found or NULL
+ * @cmd: the cmd to find
+ *
+ * Return: full path of cmd if found or NULL
  */
 char *find_path(info_t *info, char *pathstr, char *cmd)
 {
@@ -62,7 +64,7 @@ char *find_path(info_t *info, char *pathstr, char *cmd)
 	}
 	while (1)
 	{
-		if(!pathstr[i] || pathstr[i] == ':')
+		if (!pathstr[i] || pathstr[i] == ':')
 		{
 			path = dup_chars(pathstr, curr_pos, i);
 			if (!*path)
