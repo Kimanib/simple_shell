14e51de44da0d32db29e957bbe1307548f48f358
muthoni kinyua <joskin074@gmail.com>
2023-05-24 15:12:45 -0700

    simple shell

--- main.c
+++ main.c
@@ -1,20 +1,22 @@
 #include "shell.h"
 
 /**
- * main - entry point of the code
- * @ac: argument count
- * @av: argument vector
- * Return: 0 if success and 1 if error
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
  */
 int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
 	int fd = 2;
 
-	asm ("mov %1, %0, \n\t"
+	asm ("mov %1, %0\n\t"
 		"add $3, %0"
-		: "=r"(fd)
-		: "r"(fd));
+		: "=r" (fd)
+		: "r" (fd));
+
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
@@ -25,7 +27,7 @@ int main(int ac, char **av)
 			if (errno == ENOENT)
 			{
 				_eputs(av[0]);
-				_eputs(": 0: can't open");
+				_eputs(": 0: Can't open ");
 				_eputs(av[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
