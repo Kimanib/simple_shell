32979032595465b671baa08840bbbd7d6f668260
muthoni kinyua <joskin074@gmail.com>
2023-05-24 15:24:15 -0700

    simple shell

--- exits.c
+++ exits.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 /**
- * *_strncpy - copies a string
- * @dest: destination he string is to be coppied to
- * @src: source of the string
- * @n: amount of character to be copied
- * Return: the concatenated string
+ **_strncpy - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
  */
-char *_strncpy(char *dest, char  *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
 	char *s = dest;
@@ -29,12 +29,13 @@ char *_strncpy(char *dest, char  *src, int n)
 	}
 	return (s);
 }
+
 /**
- * *_strncat -concatenates 2 strings
- * @dest: first string
- * @src: second string
- * @n: amount of bytes to be used maximumly
- * Return: concatenated string
+ **_strncat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
  */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -55,11 +56,12 @@ char *_strncat(char *dest, char *src, int n)
 		dest[i] = '\0';
 	return (s);
 }
+
 /**
- * *_strchr -locates a character in a string
- * @s: string to be parsed
- * @c: the character to look for
- * Return: (s) -pointer to a memory area of s
+ **_strchr - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
  */
 char *_strchr(char *s, char c)
 {
